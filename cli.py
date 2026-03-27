2b9978e1384fbff1c930a480a18e39bf08e92a36
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2026-03-27 09:47:28 +0100

    Refactor class UntisPlaner to split CLI elements in separate file.

---
+++ cli.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+"""
+A simple command line tool to plan appointments for multiple teachers using
+timetables from WebUntis. It fetches the timetables of specified teachers and
+displays a weekly overview highlighting busy slots.
+"""
+
+import locale
+import logging
+import readline
+import configparser
+from datetime import datetime, timedelta
+
+from rich.console import Console
+from rich.table import Table
+
+import untisplaner
+
+
+def choose_teachers(list_of_all_teachers):
+    """
+    Prompt user to input teacher names with auto-completion.
+    """
+    teacher_names = [t.long_name for t in list_of_all_teachers]
+    teachers_to_plan = []
+
+    # set up auto-completion for teacher names
+    def completer(text, state):
+        options = [i for i in teacher_names if i.casefold().startswith(text.casefold())]
+        if state < len(options):
+            return options[state]
+        return None
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer)
+
+    while True:
+        teacher = input('Input teacher name (leave empty to finish): ')
+        if not teacher:
+            break
+        if teacher in teacher_names:
+            teachers_to_plan.append(teacher)
+        else:
+            logging.info(f"Teacher '{teacher}' not found. Please try again.")
+    return teachers_to_plan
+
+
+def output_lessons(lessons: dict, start: datetime.date, end: datetime.date):
+    """
+    Display the lessons in a formatted table using rich.
+    """
+    # create two dimensional array with "days" as columns and "hours" as
+    # rows beginning at index 1
+    days = (end - start).days + 1
+    timetable = [[[] for _ in range(10)] for _ in range(days)]
+    for date, teachers in lessons.items():
+        day = date.day - start.day - 1
+        match date:
+            case datetime(hour=8, minute=0):
+                hour = 1
+            case datetime(hour=8, minute=45):
+                hour = 2
+            case datetime(hour=9, minute=50):
+                hour = 3
+            case datetime(hour=10, minute=35):
+                hour = 4
+            case datetime(hour=11, minute=40):
+                hour = 5
+            case datetime(hour=12, minute=25):
+                hour = 6
+            case datetime(hour=13, minute=30):
+                hour = 7
+            case datetime(hour=14, minute=15):
+                hour = 8
+            case datetime(hour=15, minute=15):
+                hour = 9
+            case _:
+                hour = 0
+        # ignore all lessons outside of daytime school hours
+        if hour:
+            timetable[day][hour].extend(teachers)
+
+    # colors in rich: https://rich.readthedocs.io/en/latest/appendix/colors.html
+    table = Table(title='[bold bright_blue]Untis Planer[/bold bright_blue]',
+                  show_lines=True, style="bright_white", header_style="bright_blue")
+    for i in range(days):
+        day_label = (start + timedelta(days=i)).strftime("%a %d.%m")
+        table.add_column(day_label, justify="center", style="cyan", no_wrap=True)
+    for hour in range(1, 10):
+        row = []
+        for day in range(days):
+            if timetable[day][hour]:
+                teachers = '\n'.join([t.long_name for t in timetable[day][hour]])
+                if len(timetable[day][hour]) > 2:
+                    row.append(f"[bright_red]{teachers}[/bright_red]")
+                elif len(timetable[day][hour]) > 1:
+                    row.append(f"[bright_yellow]{teachers}[/bright_yellow]")
+                else:
+                    row.append(f"[bright_green]{teachers}[/bright_green]")
+            else:
+                row.append("")
+        table.add_row(*row)
+
+    console = Console()
+    console.print(table)
+
+
+def main():
+    """
+    Main function to read config and start the UntisPlaner.
+    """
+    locale.setlocale(locale.LC_ALL, '')
+    logging.basicConfig(level=logging.INFO)
+    logging.debug("Reading config file")
+    configfile = 'webuntis-config.ini'
+    config = configparser.ConfigParser()
+    config.read(configfile)
+    cred = config['credentials']
+    up = untisplaner.UntisPlaner(cred['user'], cred['password'], cred['server'], cred['school'])
+    start = datetime.fromisoformat('2026-04-04').date()
+    end = start + timedelta(days=7)
+    up.set_start_and_end(start, end)
+    teachers_to_plan = choose_teachers(up.get_list_of_teachers())
+    all_lessons = up.plan_week(teachers_to_plan)
+    output_lessons(all_lessons, start, end)
+
+
+if __name__ == "__main__":
+    main()
--- untisplaner.py
+++ untisplaner.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python3
 
 """
-A simple command line tool to plan appointments for multiple teachers using
-timetables from WebUntis. It fetches the timetables of specified teachers and
-displays a weekly overview highlighting busy slots.
+A library for fetching timetables for multiple teachers from WebUntis.
 """
 
-import locale
 import logging
 import datetime
-import configparser
 from collections import defaultdict
-import readline
 
 import webuntis
 # Doc: https://python-webuntis.readthedocs.io/en/latest/objects.html
 import webuntis.objects
-from rich.console import Console
-from rich.table import Table
 
 
 class UntisPlaner:
@@ -31,10 +23,9 @@ class UntisPlaner:
             server=server,
             school=school,
             useragent="untisplaner")
-        self.days = 5
         self.session.login()
         self.start = datetime.datetime.now().date()
-        self.end = (datetime.datetime.now() + datetime.timedelta(days=self.days)).date()
+        self.end = (datetime.datetime.now() + datetime.timedelta(days=7)).date()
         self.list_of_all_teachers = None
         self.current_school_year = None
 
@@ -61,33 +52,6 @@ class UntisPlaner:
             self.list_of_all_teachers = self.session.teachers()
         return self.list_of_all_teachers
 
-    def choose_teachers(self):
-        """
-        Prompt user to input teacher names with auto-completion.
-        """
-        list_of_all_teachers = self.get_list_of_teachers()
-        teacher_names = [t.long_name for t in list_of_all_teachers]
-        teachers_to_plan = []
-
-        # set up auto-completion for teacher names
-        def completer(text, state):
-            options = [i for i in teacher_names if i.casefold().startswith(text.casefold())]
-            if state < len(options):
-                return options[state]
-            return None
-        readline.parse_and_bind("tab: complete")
-        readline.set_completer(completer)
-
-        while True:
-            teacher = input('Input teacher name (leave empty to finish): ')
-            if not teacher:
-                break
-            if teacher in teacher_names:
-                teachers_to_plan.append(teacher)
-            else:
-                logging.info(f"Teacher '{teacher}' not found. Please try again.")
-        return teachers_to_plan
-
     def plan_week(self, teachers_to_plan):
         """
         Fetch the timetables for the specified teachers and sort them by lesson
@@ -112,7 +76,7 @@ class UntisPlaner:
         assert start_date <= end <= end_date, f"End date not in current school year {end}"
         # getting the timetable for the specified teacher and the given date range
         self.set_start_and_end(start, end)
-        tt = self.session.timetable(start=start, end=end, teacher=teacher)
+        tt = self.session.timetable(start=start-datetime.timedelta(days=1), end=end, teacher=teacher)
         logging.debug(f"Fetched {len(tt)} periods for {teacher.long_name} from {start} to {end}")
         return tt
 
@@ -132,83 +96,3 @@ class UntisPlaner:
                     # more fields in data:
                     # po.start, po.end, po.klassen, po.teachers, po.rooms, po.subjects, po.code
         return lessons
-
-    def output_lessons(self, lessons: dict):
-        """
-        Display the lessons in a formatted table using rich.
-        """
-        # create two dimensional array with "days" as columns and "hours" as
-        # rows beginning at index 1
-        timetable = [[[] for _ in range(10)] for _ in range(self.days)]
-        for date, teachers in lessons.items():
-            day = date.day - self.start.day - 1
-            match date:
-                case datetime.datetime(hour=8, minute=0):
-                    hour = 1
-                case datetime.datetime(hour=8, minute=45):
-                    hour = 2
-                case datetime.datetime(hour=9, minute=50):
-                    hour = 3
-                case datetime.datetime(hour=10, minute=35):
-                    hour = 4
-                case datetime.datetime(hour=11, minute=40):
-                    hour = 5
-                case datetime.datetime(hour=12, minute=25):
-                    hour = 6
-                case datetime.datetime(hour=13, minute=30):
-                    hour = 7
-                case datetime.datetime(hour=14, minute=15):
-                    hour = 8
-                case datetime.datetime(hour=15, minute=15):
-                    hour = 9
-                case _:
-                    hour = 0
-            # ignore all lessons outside of daytime school hours
-            if hour:
-                timetable[day][hour].extend(teachers)
-
-        # colors in rich:
-        # https://rich.readthedocs.io/en/latest/appendix/colors.html
-        table = Table(title='[bold bright_blue]Untis Planer[/bold bright_blue]',
-                      show_lines=True, style="bright_white", header_style="bright_blue")
-        for i in range(self.days):
-            day_label = (self.start + datetime.timedelta(days=i)).strftime("%a %d.%m")
-            table.add_column(day_label, justify="center", style="cyan", no_wrap=True)
-        for hour in range(1, 10):
-            row = []
-            for day in range(self.days):
-                if timetable[day][hour]:
-                    teachers = '\n'.join([t.long_name for t in timetable[day][hour]])
-                    if len(timetable[day][hour]) > 2:
-                        row.append(f"[bright_red]{teachers}[/bright_red]")
-                    elif len(timetable[day][hour]) > 1:
-                        row.append(f"[bright_yellow]{teachers}[/bright_yellow]")
-                    else:
-                        row.append(f"[bright_green]{teachers}[/bright_green]")
-                else:
-                    row.append("")
-            table.add_row(*row)
-
-        console = Console()
-        console.print(table)
-
-
-def main():
-    """
-    Main function to read config and start the UntisPlaner.
-    """
-    locale.setlocale(locale.LC_ALL, '')
-    logging.basicConfig(level=logging.INFO)
-    logging.debug("Reading config file")
-    configfile = 'webuntis-config.ini'
-    config = configparser.ConfigParser()
-    config.read(configfile)
-    cred = config['credentials']
-    up = UntisPlaner(cred['user'], cred['password'], cred['server'], cred['school'])
-    teachers_to_plan = up.choose_teachers()
-    all_lessons = up.plan_week(teachers_to_plan)
-    up.output_lessons(all_lessons)
-
-
-if __name__ == "__main__":
-    main()
